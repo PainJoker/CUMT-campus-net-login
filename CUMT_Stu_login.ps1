882af835f2834363a0a247a3ddf33b0851a628ac
PainJoker <painjoker42@gmail.com>
2023-11-29 21:37:40 +0800

    初步实现网络连接功能

---
+++ CUMT_Stu_login.ps1
@@ -0,0 +1,9 @@
+
+$WLANUserIP = ((Get-NetIPAddress -AddressFamily IPv4).IPv4Address
+                | Select-String -Pattern '10.\d+.\d+.\d+')
+$Account = ''
+$Password = ''
+$ServiceProvider = ''
+$AuthUrl = "http://10.2.5.251:801/eportal/?c=Portal&a=login&callback=dr1699582158609&login_method=1&user_account=$Account%40$ServiceProvider&user_password=$Password&wlan_user_ip=$WLANUserIP&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.0&_=1699582148"
+
+Invoke-WebRequest -Uri $AuthUrl
