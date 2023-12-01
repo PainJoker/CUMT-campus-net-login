42b3d4273f8c51a77d524fbbecf4a96c1074e420
PainJoker <painjoker42@gmail.com>
2023-12-01 23:11:46 +0800

    增加简单的用户提示

--- CUMT_Stu_login.ps1
+++ CUMT_Stu_login.ps1
@@ -1,9 +1,16 @@
-
+# 获取本机 IP 地址
 $WLANUserIP = ((Get-NetIPAddress -AddressFamily IPv4).IPv4Address
-                | Select-String -Pattern '10.\d+.\d+.\d+')
-$Account = ''
-$Password = ''
-$ServiceProvider = ''
-$AuthUrl = "http://10.2.5.251:801/eportal/?c=Portal&a=login&callback=dr1699582158609&login_method=1&user_account=$Account%40$ServiceProvider&user_password=$Password&wlan_user_ip=$WLANUserIP&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.0&_=1699582148"
+                | Select-String -Pattern '10\.\d+\.\d+\.\d+')
 
+# 在引号中填入校园网账号
+$Account = ''
+# 在引号中填入校园网密码
+$Password = ''
+# 根据校园网账号运营商类型填入：
+# 中国移动 --> cmcc
+# 中国联通 --> unicom
+# 中国电信 --> telecom
+$ServiceProvider = ''
+
+$AuthUrl = "http://10.2.5.251:801/eportal/?c=Portal&a=login&callback=dr1699582158609&login_method=1&user_account=$Account%40$ServiceProvider&user_password=$Password&wlan_user_ip=$WLANUserIP&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=3.0&_=1699582148"
 Invoke-WebRequest -Uri $AuthUrl
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # CUMT-campus-net-login
-Log in CUMT campus net for students with PowerShell script.
+使用PowerShell脚本实现了基本的校园网登录的功能，需要根据代码中的注释填写登录的必须信息。
+
+## Prerequisite
+
+如果是第一次使用，需要在Windows系统上安装PowerShell。参考链接: *[installing-powershell-on-windows](https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.4)*.
+
+> ***注意***：首次运行PowerShell脚本需要运行以下命令将PowerShell的执行策略更改为远程签名。
+>
+> ```powershell
+> Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+> ```
+
+## Some words to say
+
+这是一次新的尝试，后续可能会加入根据网络名称触发的逻辑。
